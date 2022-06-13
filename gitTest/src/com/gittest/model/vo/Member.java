d677da7d30d4926b4d83dc202a0a4d827d3c1ca0
Zambo <Zambo@DESKTOP-JOSQK8I>
2022-06-13 11:20:21 +0900

    toString메소드 추가

--- gitTest/src/com/gittest/model/vo/Member.java
+++ gitTest/src/com/gittest/model/vo/Member.java
@@ -24,6 +24,11 @@ public class Member {
 	public void setAge(int age) {
 		this.age = age;
 	}
+
+	@Override
+	public String toString() {
+		return "Member [name=" + name + ", age=" + age + "]";
+	}
 	
 	
 	
